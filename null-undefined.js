2e5bb43abc5596cd73da75af0260acfb201608a4
theyogesh10 <yogeshpahade10@gmail.com>
2024-03-14 10:28:51 +0530

    null undefined

---
+++ null-undefined.js
@@ -0,0 +1,11 @@
+var x;
+console.log(x);   //undefined
+
+var y = null;
+console.log(y);   //null
+
+
+var obj = {
+    x: 1
+};
+obj = null; // because we want memory, the only way to access memory
