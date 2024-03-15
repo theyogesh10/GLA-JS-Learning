4d95c42c89816e046aa28f913b44fe7f16f3f202
theyogesh10 <yogeshpahade10@gmail.com>
2024-03-15 16:00:07 +0530

    loop - for and while

---
+++ looping.js
@@ -0,0 +1,16 @@
+//for loop 
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("after loop")
+
+
+//while loop
+
+var j = 1;
+while( j <= 10 ) {
+    console.log(j);
+    j++;
+}
