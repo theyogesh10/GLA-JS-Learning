102c58543ed2734c72a41b2316a44e39f9a86947
theyogesh10 <yogeshpahade10@gmail.com>
2024-03-15 15:37:35 +0530

    if else

---
+++ branching-ifelse.js
@@ -0,0 +1,13 @@
+//if else
+
+var priceOfPhone = 42000, bankBalance = 60000;
+
+if( priceOfPhone <  bankBalance ) {
+    console.log("All ok, process payment"); 
+} else{
+    console.log("Sorry, you do not have sufficient funds");
+}
+
+console.log("Trasection over");
+
+
