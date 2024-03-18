98ca33b592535bbb1470324f6ef21d1ff6160fd6
theyogesh10 <yogeshpahade10@gmail.com>
2024-03-18 22:39:25 +0530

    function iife return value

---
+++ function-iife-return-value.js
@@ -0,0 +1,11 @@
+//iief return value
+const someVariable = (function() {      
+    return 100;
+}());
+console.log( someVariable );
+
+
+//function expression
+const sum = function(x, y) {           
+    return x + y;
+};
