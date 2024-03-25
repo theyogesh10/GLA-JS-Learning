9a85b25c2bc355c1b719d9b6242d14dc399d463b
theyogesh10 <yogeshpahade10@gmail.com>
2024-03-25 21:12:25 +0530

    math class methods

---
+++ math-class-methods.js
@@ -0,0 +1,28 @@
+console.log( Math);
+
+console.log( Math.random());
+
+console.log( Math.min(10, 50, 30, 70, 28, 48, 8, 90 ) );
+console.log( Math.max(10, 50, 30, 70, 28, 48, 8, 90 ) );
+
+
+var numbers = [10, 50, 30, 70, 28, 48, 8, 90];
+console.log( Math.min.apply(null, numbers) );
+
+
+console.log( Math.ceil(3.5) );
+console.log( Math.ceil(5) );
+
+console.log( Math.floor(3.5) );
+console.log( Math.floor(5) );
+
+console.log( Math.abs(3.5) );
+console.log( Math.abs(5) );
+
+console.log( Math.pow(3, 4) ); 
+
+console.log( Math.round(3.6) ); 
+console.log( Math.round(3.4) ); 
+
+
+
