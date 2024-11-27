b1f590d5c06d6dd3fc28854e384b3d670d0047ae
theyogesh10 <yogeshpahade10@gmail.com>
2024-11-28 00:22:17 +0530

    map

---
+++ es2015/12-map.js
@@ -0,0 +1,21 @@
+const numbers = new Map( [ [ 1,"one" ], [ 2, "two" ], [ 3, "three" ] ] );
+
+console.log( numbers.size );
+console.log( numbers.entries() );
+console.log( numbers.keys() );
+console.log( numbers.values() );
+
+numbers.set( 40, "Forty" );
+console.log( numbers );
+
+console.log( numbers.get( 3 ) );
+
+numbers.delete( 2 );
+console.log( numbers );
+
+// numbers.clear();
+// console.log( numbers );
+
+for( let [ key, value ] of numbers) {
+    console.log( `${key} = ${value}` )
+}
