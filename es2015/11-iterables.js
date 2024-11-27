8bfd1d749588817814881e59bc46457163c8be0e
theyogesh10 <yogeshpahade10@gmail.com>
2024-11-27 22:45:47 +0530

    iterables

---
+++ es2015/11-iterables.js
@@ -0,0 +1,16 @@
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday'
+];
+ 
+// for...of
+for( let item of days ) {
+    console.log( item );
+} 
+
+const greeting = 'Good morning';
+for( let char of greeting ) {
+    console.log( char );
+}
