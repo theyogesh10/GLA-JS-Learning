fc0d8227a4c239672360594cead3092eaf37015a
theyogesh10 <yogeshpahade10@gmail.com>
2024-11-30 23:06:31 +0530

    fetch get post

---
+++ es2015/18-fetch.js
@@ -0,0 +1,28 @@
+// GET data
+fetch( 'https://jsonplaceholder.typicode.com/todos/1' )
+.then( response => response.json() )
+.then( value => console.log( value ) )
+.catch( error => console.error( error.message ) );
+
+
+// Data for POST 
+const newTodo = {
+    "userId": 1,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  }
+  
+// POST data
+fetch( 'https://jsonplaceholder.typicode.com/todos', 
+    {
+        method: 'POST',
+        body: JSON.stringify( newTodo )
+    },
+    {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+)
+.then( response => response.json() )
+.then( value => console.log( value ) )
